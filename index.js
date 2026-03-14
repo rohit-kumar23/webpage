4b859a1b5b1dfc1b07ab0d1d3098833ca6b2c590
Rohit <www.23ohit@gmail.com>
2026-03-14 18:39:11 +0530

    updated

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const updateTime = () => {
   const time = date.toLocaleTimeString("en-GB", {
     hour: "2-digit",
     minute: "2-digit",
+    second: "2-digit",
     timeZone: timeZone,
   });
 
